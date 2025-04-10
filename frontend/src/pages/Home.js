1bc8cc2da9787d2c21f50a775c013a2d49a7443b
Larryhurtado <lhurtadoh@estudiante.uniajc.edu.co>
2025-04-10 08:04:15 -0500

    Definimos la página "Home" como punto de partida al ejecutar "npm start"

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+        <h1>Bienvenido a la página de inicio</h1>
+        {/* Resto de tu contenido */}
+        </div>
+    );
+};
+
+export default Home;
