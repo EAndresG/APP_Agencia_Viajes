8abc143d40bb7e4fd6f0b2e8b97f46f0c370d274
Larryhurtado <lhurtadoh@estudiante.uniajc.edu.co>
2025-04-23 10:45:16 -0500

    Se descargo "Morgan" y se conecta de manera exitosa backend con la base de datos

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const routes = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const app = require('./srs/app');
+const app = express();
 
 // Middlewares
+app.use(morgan("dev")); // Registrar solicitudes HTTP
 app.use(cors());
-app.use(morgan('dev'));
 app.use(express.json());
 
 // Rutas
-app.use('/api', routes);
+const authRoutes = require("./src/routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
 // Iniciar servidor
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 
